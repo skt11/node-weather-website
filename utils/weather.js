c89140a9883718b6e65d252d6caa3ec3bfc8c689
saikat <saikata95@gmail.com>
2019-11-22 19:31:50 +0530

    API info added to about page

--- utils/weather.js
+++ utils/weather.js
@@ -12,7 +12,8 @@ const forecast = (latitude, longitude, callBack) => {
                 summary: body.daily.data[0].summary,
                 temperature: body.currently.temperature,
                 precipProbability: body.currently.precipProbability,
-                forecast: `${body.daily.data[0].summary} It is ${body.currently.temperature} degrees outside with a ${body.currently.precipProbability}% chance of rain.`
+                forecast: `${body.daily.data[0].summary} It is ${body.currently.temperature} degrees outside with a ${body.currently.precipProbability}% chance of rain.
+Highest temperature hitting ${body.daily.data[0].temperatureHigh} degrees and lowest hittng ${body.daily.data[0].temperatureLow} degrees for the day.`
             });
         }
     });
